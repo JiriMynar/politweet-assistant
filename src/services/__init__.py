44a1b79ede434a943f3f9376690a1789a50c77e3
JiriMynar <avici@seznam.cz>
2025-05-28 19:41:57 +0200

    Delete src directory

--- src/services/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Inicializační soubor pro služby.
-
-Tento modul poskytuje přístup ke službám aplikace.
-"""
-
-from src.services.openai_service import OpenAIService
-
-__all__ = ["OpenAIService"]
